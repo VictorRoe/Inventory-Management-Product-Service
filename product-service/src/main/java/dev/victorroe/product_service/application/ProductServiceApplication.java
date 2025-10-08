4aaac2237db88a5fd29e4747551b7259cab7268a
VictorRoe <victorroe819@gmail.com>
2025-10-08 17:09:21 -0500

    Change: .properties to .yaml

--- product-service/src/main/java/dev/victorroe/product_service/application/ProductServiceApplication.java
+++ product-service/src/main/java/dev/victorroe/product_service/application/ProductServiceApplication.java
@@ -1,4 +1,4 @@
-package dev.victorroe.product_service;
+package applications.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
