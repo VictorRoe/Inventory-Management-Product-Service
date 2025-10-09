63ae4f9e8760350c80f7e477f4f87b0bde1acb0f
VictorRoe <victorroe819@gmail.com>
2025-10-09 11:59:41 -0500

    add: Product Model

--- product-service/src/main/java/dev/victorroe/product_service/domain/model/Product.java
+++ product-service/src/main/java/dev/victorroe/product_service/domain/model/Product.java
@@ -1,4 +1,20 @@
 package dev.victorroe.product_service.domain.model;
 
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 public class Product {
+    
+    private String id;
+    private String SKU;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer quantity;
 }
