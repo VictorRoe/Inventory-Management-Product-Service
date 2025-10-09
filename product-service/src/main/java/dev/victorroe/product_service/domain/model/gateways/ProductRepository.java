a7d5708e6fd170f5da3075d6afe81c1e06d9d257
VictorRoe <victorroe819@gmail.com>
2025-10-09 13:02:03 -0500

    add: registerProduct contract

--- product-service/src/main/java/dev/victorroe/product_service/domain/model/gateways/ProductRepository.java
+++ product-service/src/main/java/dev/victorroe/product_service/domain/model/gateways/ProductRepository.java
@@ -1,4 +1,9 @@
 package dev.victorroe.product_service.domain.model.gateways;
 
+import dev.victorroe.product_service.domain.model.Product;
+import reactor.core.publisher.Mono;
+
 public interface ProductRepository {
+
+    Mono<Product> registerProduct(Product product);
 }
