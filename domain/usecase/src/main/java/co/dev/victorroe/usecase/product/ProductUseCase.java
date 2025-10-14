a821446485e54bb41e265430e8343a54e42890fa
VictorRoe <victorroe819@gmail.com>
2025-10-14 11:25:25 -0500

    add: Model Use Case

--- domain/usecase/src/main/java/co/dev/victorroe/usecase/product/ProductUseCase.java
+++ domain/usecase/src/main/java/co/dev/victorroe/usecase/product/ProductUseCase.java
@@ -1,6 +1,17 @@
 package co.dev.victorroe.usecase.product;
 
+import co.dev.victorroe.model.product.Product;
+import co.dev.victorroe.model.product.gateways.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
 @RequiredArgsConstructor
 public class ProductUseCase {
+
+    private final ProductRepository productRepository;
+
+    Mono<Product> create(Product product){
+        return productRepository.create(product);
+    }
+
 }
