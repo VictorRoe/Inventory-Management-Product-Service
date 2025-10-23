6bc3b5fc5f244901fa1922318cd3de54f12d1425
VictorRoe <victorroe819@gmail.com>
2025-10-23 16:44:48 -0500

    update: UseCases using with SQS

--- domain/usecase/src/main/java/co/dev/victorroe/usecase/product/RemoveStockUseCase.java
+++ domain/usecase/src/main/java/co/dev/victorroe/usecase/product/RemoveStockUseCase.java
@@ -2,6 +2,9 @@ package co.dev.victorroe.usecase.product;
 
 import static java.util.Objects.isNull;
 
+import co.dev.victorroe.model.inventorymovement.InventoryMovement;
+import co.dev.victorroe.model.inventorymovement.TransactionType;
+import co.dev.victorroe.model.inventorymovement.gateways.InventoryMovementRepository;
 import co.dev.victorroe.model.product.Product;
 import co.dev.victorroe.model.product.StockOutputType;
 import co.dev.victorroe.model.product.gateways.ProductRepository;
@@ -9,23 +12,28 @@ import co.dev.victorroe.usecase.product.exception.InsufficientStockException;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
+import java.util.logging.Logger;
+
 
 @RequiredArgsConstructor
-public class RemoveStockUseCase implements IRemoveStockUseCase{
+public class RemoveStockUseCase implements IRemoveStockUseCase {
 
     private final ProductRepository repository;
+    private final InventoryMovementRepository movementRepository;
+    private final Logger log = Logger.getLogger(RemoveStockUseCase.class.getName());
 
     @Override
     public Mono<Product> removeStock(Long id, Long quantity, StockOutputType type) {
 
-        if (isNull(quantity) || quantity <= 0){
+        if (isNull(quantity) || quantity <= 0) {
             return Mono.error(new IllegalArgumentException("La cantidad a restar debe ser mayor que cero"));
         }
 
         return repository.findById(id)
                 .switchIfEmpty(Mono.error(new RuntimeException("Producto no encontrado con ID: " + id)))
                 .flatMap(ifProductExist -> {
-                    if (ifProductExist.getStock() < quantity){
+                    if (ifProductExist.getStock() < quantity) {
                         return Mono.error(new InsufficientStockException("Stock insuficiente. Disponible: " + ifProductExist.getStock()));
                     }
 
@@ -36,6 +44,23 @@ public class RemoveStockUseCase implements IRemoveStockUseCase{
                             .build();
 
                     return repository.update(updateProduct);
+                })
+                .doOnSuccess(updatedProduct -> {
+                    TransactionType txType = (type == StockOutputType.SALE) ? TransactionType.STOCK_REMOVED_SALE : TransactionType.STOCK_REMOVED_WASTE;
+                    String message = (type == StockOutputType.SALE) ? "Salida por venta" : "Salida por merma";
+
+                    InventoryMovement movement = InventoryMovement.builder()
+                            .productId(updatedProduct.getId())
+                            .transactionType(txType)
+                            .quantity(-quantity)
+                            .transactionTime(LocalDateTime.now())
+                            .userId("system-user")
+                            .message(message)
+                            .build();
+
+                    movementRepository.notifyMovement(movement)
+                            .doOnError(e -> log.warning("Error al publicar evento SQS de salida de stock"))
+                            .subscribe();
                 });
     }
 }
--- domain/usecase/src/main/java/co/dev/victorroe/usecase/product/UpdateProductUseCase.java
+++ domain/usecase/src/main/java/co/dev/victorroe/usecase/product/UpdateProductUseCase.java
@@ -1,17 +1,24 @@
 package co.dev.victorroe.usecase.product;
 
+import co.dev.victorroe.model.inventorymovement.InventoryMovement;
+import co.dev.victorroe.model.inventorymovement.TransactionType;
+import co.dev.victorroe.model.inventorymovement.gateways.InventoryMovementRepository;
 import co.dev.victorroe.model.product.Product;
 import co.dev.victorroe.model.product.gateways.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @RequiredArgsConstructor
 public class UpdateProductUseCase implements IUpdateProductUseCase {
 
     private final ProductRepository repository;
+    private final InventoryMovementRepository movementRepository;
+    private final Logger log = Logger.getLogger(UpdateProductUseCase.class.getName());
 
     @Override
     public Mono<Product> update(Long id, Optional<BigDecimal> newPrice, Optional<String> newDescription) {
@@ -26,6 +33,22 @@ public class UpdateProductUseCase implements IUpdateProductUseCase {
                     Product updateProduct = productBuilder.build();
 
                     return repository.update(updateProduct);
+                })
+
+                .doOnSuccess(updateProduct -> {
+                    InventoryMovement movement = InventoryMovement.builder()
+                            .productId(updateProduct.getId())
+                            .transactionType(TransactionType.PRODUCT_UPDATED)
+                            .quantity(0L)
+                            .transactionTime(LocalDateTime.now())
+                            .userId("system-user")
+                            .message("Actualización de datos precio: " + updateProduct.getPrice() + " | Descripcion: " +  updateProduct.getDescription())
+                            .build();
+
+
+                    movementRepository.notifyMovement(movement)
+                            .doOnError(e -> log.warning("Error al publicar evento SQS de actualización"))
+                            .subscribe();
                 });
     }
 }
